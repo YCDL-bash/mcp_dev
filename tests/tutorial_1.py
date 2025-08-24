c44c8e00588f323a7271730c2bc0b529bdc48521
YCDL-bash <davidtheycl@naver.com>
2025-08-24 15:47:18 +0900

    tutorial_1.py 테스트 파일 추가

---
+++ tests/tutorial_1.py
@@ -0,0 +1,16 @@
+from mcp.server.fastmcp import FastMCP
+
+# MCP 서버 생성하기
+mcp = FastMCP(name="tutoral_1")
+
+@mcp.tool()
+def echo(message: str) -> str:
+    """
+    입력받은 메지시를 그대로 반환하는 도구입니다.
+    """
+    return message + " 라는 메시지가 입력되었습니다. 안 찍어 볼 수 없죠! hello world!"
+
+# 서버 실행하기
+if __name__ == "__main__":
+    mcp.run()
+    
